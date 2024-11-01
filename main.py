a9f5561a340a94ba64b7d995cfa5bdd073a28c9f
Wesley Thauan <thauanblox@gmail.com>
2024-11-01 10:37:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+class Animal:
+    def falar(self):
+        print("Esse animal esta fazendo um som")
+    
+class Cachorro:
+    def falar(self):
+        print (")O Cachorro late")
+    
+class Gato:
+    def falar(self):
+        print ("O Gato Mia")
+    
+animal = Animal()
+animal.falar()
+
+cachorro = Cachorro()
+cachorro.falar()
+
+gato = Gato()
+gato.falar()
